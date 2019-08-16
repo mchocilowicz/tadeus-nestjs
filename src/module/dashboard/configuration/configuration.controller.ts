98680cb2049df71bbbd99be99cebac6878b19c53
Marcin Chocilowicz <m.chocilowicz@gmail.com>
2019-08-16 11:19:46 +0200

    update configuration

--- src/module/dashboard/configuration/configuration.controller.ts
+++ src/module/dashboard/configuration/configuration.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Body, Controller, Get, Post, Put } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { Configuration } from "../../../database/entity/configuration.entity";
 
 @Controller()
@@ -6,26 +6,17 @@ export class ConfigurationController {
 
     @Post()
     async save(@Body() dto: any) {
-        let count = await Configuration.count();
-        if (count === 1) {
-            throw new BadRequestException('configuration_exists')
-        } else {
-            let config = new Configuration();
-            this.mapDtoToEntity(dto, config);
-            await config.save();
+        let config = await Configuration.findOne({type: 'MAIN'});
+        if (!config) {
+            config = new Configuration();
         }
+        this.mapDtoToEntity(dto, config);
+        await config.save();
     }
 
     @Get()
     async getConfiguration() {
         return await Configuration.findOne({type: 'MAIN'});
-    }
-
-    @Put()
-    async updateConfiguration(@Body() dto: any) {
-        let config = await Configuration.findOne({type: 'MAIN'});
-        this.mapDtoToEntity(dto, config);
-        await config.save();
     }
 
     mapDtoToEntity(dto: any, config: Configuration) {
