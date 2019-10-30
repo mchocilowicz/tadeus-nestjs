689dfe389d48c6c79268b0676c4f6c72d5a2757a
Marcin Chocilowicz <m.chocilowicz@gmail.com>
2019-10-30 19:38:38 +0100

    HEROKU #3

--- src/module/app/app.module.ts
+++ src/module/app/app.module.ts
@@ -109,6 +109,7 @@ const routes: Routes = [
             useFactory: () => ({
                 url: process.env.DATABASE_URL,
                 type: "postgres",
+                ssl: true,
                 schema: 'tds',
                 entities: [
                     join(__dirname, "../../**/*.entity{.ts,.js}")
