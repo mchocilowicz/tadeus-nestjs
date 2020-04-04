4120debd51d008142602f4396a5c62b859439e65
Marcin Chocilowicz <m.chocilowicz@gmail.com>
2020-04-04 22:46:13 +0200

    reports #5

--- src/module/dashboard/reports/reports.controller.ts
+++ src/module/dashboard/reports/reports.controller.ts
@@ -11,10 +11,12 @@ export class ReportsController {
 
     @Get('dates')
     async getReportsDates() {
-        return await Transaction.createQueryBuilder('t')
+        const dates = await Transaction.createQueryBuilder('t')
             .select("to_char(t.createdAt,'YYYY-MM')", 'date')
             .groupBy("to_char(t.createdAt,'YYYY-MM')")
             .getRawMany();
+
+        return dates.map(e => e.date);
     }
 
     @Get()
@@ -35,38 +37,38 @@ export class ReportsController {
 
         let ngoList = _.uniqBy(transactions.map(elem => {
             let obj = {
-                ID: elem.ngo.ID,
-                name: elem.ngo.name,
+                ID: elem['ngo'].ID,
+                name: elem['ngo'].name,
                 usersCount: 0,
                 donations: 0,
-                tradingPointsCount: 0,
-                tradingPoint: []
+                count: 0,
+                list: []
             };
             return obj;
         }), "ID");
 
-        let groupedTransactionByNgo = _.groupBy(transactions, (elem: Transaction) => elem.ngo.ID);
+        let groupedTransactionByNgo = _.groupBy(transactions, (elem: Transaction) => elem['ngo'].ID);
         Object.keys(groupedTransactionByNgo).forEach((value: string) => {
             const foundNgo = ngoList.find((n: any) => n.ID === value);
             if (foundNgo) {
                 const transactions: Transaction[] = groupedTransactionByNgo[value];
                 foundNgo.usersCount = _.uniq(transactions.map(t => t.user.id)).length;
                 foundNgo.donations = transactions.reduce((previousValue, currentValue) => previousValue + currentValue.ngoDonation, 0);
-                foundNgo.tradingPointsCount = _.uniq(transactions.map(t => t.tradingPoint.id)).length;
+                foundNgo.count = _.uniq(transactions.map(t => t['tradingPoint'].id)).length;
                 const tradingPoints = _.uniqBy(transactions.map(elem => {
                     return {
-                        ID: elem.tradingPoint.ID,
-                        name: elem.tradingPoint.name,
-                        transactionsCount: 0
+                        ID: elem['tradingPoint'].ID,
+                        name: elem['tradingPoint'].name,
+                        count: 0
                     }
                 }), 'ID');
                 tradingPoints.forEach((point: any) => {
-                    point.transactionsCount = transactions.filter(e => e.tradingPoint.ID === point.ID).length;
+                    point.count = transactions.filter(e => e['tradingPoint'].ID === point.ID).length;
                 });
-                foundNgo.tradingPoint = _.take(tradingPoints.sort((a: any, b: any) => {
-                    if (a.transactionsCount > b.transactionsCount) {
+                foundNgo.list = _.take(tradingPoints.sort((a: any, b: any) => {
+                    if (a.count > b.count) {
                         return -1;
-                    } else if (a.transactionsCount < b.transactionsCount) {
+                    } else if (a.count < b.count) {
                         return 1;
                     } else {
                         return 0
@@ -78,38 +80,38 @@ export class ReportsController {
 
         let pointList = _.uniqBy(transactions.map(elem => {
             let obj = {
-                ID: elem.tradingPoint.ID,
-                name: elem.tradingPoint.name,
+                ID: elem['tradingPoint'].ID,
+                name: elem['tradingPoint'].name,
                 usersCount: 0,
                 donations: 0,
-                ngoCount: 0,
-                ngoList: []
+                count: 0,
+                list: []
             };
             return obj;
         }), "ID");
 
-        let groupedTransactionByPoint = _.groupBy(transactions, (elem: Transaction) => elem.tradingPoint.ID);
+        let groupedTransactionByPoint = _.groupBy(transactions, (elem: Transaction) => elem['tradingPoint'].ID);
         Object.keys(groupedTransactionByPoint).forEach((value: string) => {
             const founPoint = pointList.find((n: any) => n.ID === value);
             if (founPoint) {
                 const transactions: Transaction[] = groupedTransactionByPoint[value];
                 founPoint.usersCount = _.uniq(transactions.map(t => t.user.id)).length;
                 founPoint.donations = transactions.reduce((previousValue, currentValue) => previousValue + currentValue.ngoDonation, 0);
-                founPoint.ngoCount = _.uniq(transactions.map(t => t.ngo.id)).length;
+                founPoint.count = _.uniq(transactions.map(t => t['ngo'].id)).length;
                 const ngos = _.uniqBy(transactions.map(elem => {
                     return {
-                        ID: elem.ngo.ID,
-                        name: elem.ngo.name,
-                        transactionsCount: 0
+                        ID: elem['ngo'].ID,
+                        name: elem['ngo'].name,
+                        count: 0
                     }
                 }), 'ID');
                 ngos.forEach((ngo: any) => {
-                    ngo.transactionsCount = transactions.filter(e => e.ngo.ID === ngo.ID).length;
+                    ngo.count = transactions.filter(e => e['ngo'].ID === ngo.ID).length;
                 });
-                founPoint.ngoList = _.take(ngos.sort((a: any, b: any) => {
-                    if (a.transactionsCount > b.transactionsCount) {
+                founPoint.list = _.take(ngos.sort((a: any, b: any) => {
+                    if (a.count > b.count) {
                         return -1;
-                    } else if (a.transactionsCount < b.transactionsCount) {
+                    } else if (a.count < b.count) {
                         return 1;
                     } else {
                         return 0
