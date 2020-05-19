1045835e7cb41b976a3284954fd0b4083f03c3e8
Marcin Chocilowicz <m.chocilowicz@gmail.com>
2020-05-19 19:38:22 +0200

    Migration file for description

---
+++ migrations/1589907750830-Add_Description_to_Trading_point.ts
@@ -0,0 +1,22 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AddDescriptionToTradingPoint1589907750830 implements MigrationInterface {
+    name = 'AddDescriptionToTradingPoint1589907750830'
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`ALTER TABLE "PARTNER" ADD "DESCRIPTION" character varying(550)`, undefined);
+        await queryRunner.query(`ALTER TABLE "USER_PAYOUT" DROP COLUMN "ACCOUNT_NUMBER"`, undefined);
+        await queryRunner.query(`ALTER TABLE "USER_PAYOUT" ADD "ACCOUNT_NUMBER" bigint NOT NULL`, undefined);
+        await queryRunner.query(`ALTER TABLE "USER" DROP COLUMN "ACCOUNT_NUMBER"`, undefined);
+        await queryRunner.query(`ALTER TABLE "USER" ADD "ACCOUNT_NUMBER" bigint`, undefined);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`ALTER TABLE "USER" DROP COLUMN "ACCOUNT_NUMBER"`, undefined);
+        await queryRunner.query(`ALTER TABLE "USER" ADD "ACCOUNT_NUMBER" integer`, undefined);
+        await queryRunner.query(`ALTER TABLE "USER_PAYOUT" DROP COLUMN "ACCOUNT_NUMBER"`, undefined);
+        await queryRunner.query(`ALTER TABLE "USER_PAYOUT" ADD "ACCOUNT_NUMBER" character varying NOT NULL`, undefined);
+        await queryRunner.query(`ALTER TABLE "PARTNER" DROP COLUMN "DESCRIPTION"`, undefined);
+    }
+
+}
